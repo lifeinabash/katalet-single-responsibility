b1f01e15d6d421b3a27eed121ed27eae0582d8aa
Harmon Wood (-) <harmon.wood@jwt.com>
2015-05-14 06:33:47 -0500

    added python and ruby code

---
+++ src/python/katalet.py
@@ -0,0 +1,4 @@
+import sys
+
+kataName = sys.argv[1]
+print 'The katalet you have selected is', kataName
---
+++ src/ruby/katalet.rb
@@ -0,0 +1,3 @@
+kataName = ARGV[0]
+output = "The katalet you have selected is %s" % kataName
+puts output
