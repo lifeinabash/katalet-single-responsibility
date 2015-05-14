1793afc71020f034a2e8dab47bc589d9f3d8d50c
Harmon Wood (-) <harmon.wood@jwt.com>
2015-05-13 22:09:19 -0500

    added java source code

---
+++ src/java/katalet.java
@@ -0,0 +1,7 @@
+class Katalet
+{
+    public static void main (String[] args) {
+        String kataName = args[0];
+        System.out.format("The katalete you have selected is %s\n", kataName);
+    }
+}
